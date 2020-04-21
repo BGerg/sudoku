c11d3f1eb65b16d80a3cbfbc6bc161332914da4c
BGerg <example@sample.com>
2020-04-22 00:54:46 +0200

    last ok

--- sudokuGame.py
+++ sudokuGame.py
@@ -1,7 +1,7 @@
 # Sudoku game
 import random
-
-
+import subprocess
+import time
 
 
 numberTable = [[0, 0, 0,  0, 0, 0,  0, 0, 0],
@@ -23,87 +23,103 @@ def rowCheck(row, number):
         return False
 
 
-def columnCheck(ind, number):
+def columnCheck(column, number):
     for i in range(9):
-        if number == numberTable[i][ind]:
+        if number == numberTable[i][column]:
             return False
     return True
 
-def origoSwitch(row, column):
-    pass
-
 def regionCheck(origo,column, number):
-    #if (row % 3) == 0: #and (column % 3) == 0:
     for i in range(3):
         for j in range(3):
-                #print(origo+i)
             if number == numberTable[origo+i][column+j]:
                 return False
     return True
 
+def origoSwitch(row, column):
+    if row < 3 and column < 3:
+        indexRow = 0
+        indexColumn = 0
+    elif row < 3 and 2 < column < 6:
+        indexRow = 0
+        indexColumn = 3
+    elif row < 3 and 5 < column :
+        indexRow = 0
+        indexColumn = 6
+    elif 2 < row < 6 and column < 3:
+        indexRow = 3
+        indexColumn = 0
+    elif 2 < row < 6 and 2 < column < 6:
+        indexRow = 3
+        indexColumn = 3
+    elif 2 < row < 6 and 5 < column:
+        indexRow = 3
+        indexColumn = 6
+    elif 5 < row and column < 3:
+        indexRow = 6
+        indexColumn = 0
+    elif 5 < row and 2 < column < 6:
+        indexRow = 6
+        indexColumn = 3
+    elif 5 < row and 5 < column:
+        indexRow = 6
+        indexColumn = 6
+
+    return indexRow,indexColumn
+
+def deleteRow(row):
+    for i in range(9):
+        numberTable[row][i] = 0
+
+total = 0
 
 
+
+
+proba = ""
 for row in range(9):
         restart = True
+
         while restart:
-            for i in range(9):
-                numberTable[row][i] = 0
-            count = 0
+            deleteRow(row)
+
             for column in range(9):
                 loopCount = 0
+
                 while numberTable[row][column] == 0:
                     loopCount += 1
 
-                    number = random.randrange(1, 10 , 1)
-                    if row < 3 and column < 3:
-                        indOne = 0
-                        indTwo = 0
-                    elif row < 3 and 2 < column < 6:
-                        indOne = 0
-                        indTwo = 3
-                    elif row < 3 and column > 5:
-                        indOne = 0
-                        indTwo = 6
-                    elif 2 < row < 6 and column < 3:
-                        indOne = 3
-                        indTwo = 0
-                    elif 2 < row < 6 and 2 < column < 6:
-                        indOne = 3
-                        indTwo = 3
-                    elif 2 < row < 6 and 5 < column:
-                        indOne = 3
-                        indTwo = 6
-                    elif 5 < row and column < 3:
-                        indOne = 6
-                        indTwo = 0
-                    elif 5 < row and 2 < column < 6:
-                        indOne = 6
-                        indTwo = 3
-                    elif 5 < row and 5 < column:
-                        indOne = 6
-                        indTwo = 6
+                    number = random.randint(1, 9)
+                    indexRow,indexColumn = origoSwitch(row, column)
 
-
-                    if rowCheck(row, number) and columnCheck(column, number) and regionCheck(indOne, indTwo, number):
+                    if rowCheck(row, number) and columnCheck(column, number) and regionCheck(indexRow, indexColumn, number):
                         numberTable[row][column] = number
-                        count += 1
-                    if count == 9:
+                        if column > 0 and numberTable[row][column-1] == 0 :
+                            proba = "WTF"
+                    if 0 not in numberTable[row]:
                         restart = False
-                    elif loopCount > 100 and count < 9:
+                    elif loopCount > 10 and 0 in numberTable[row]:
                         break
 
-                    print(f"\n   loop:{loopCount}    column: {column}             Debug Table     {number}             ")
+
+
+
+
+
+
+                    print(f"\n    Debug Table  {proba}   Random number {number}             ")
                     for g in range(9):
                         if (g % 3) == 0:
                             print("\n")
                         print(numberTable[g])
-                    print(count)
+                    
+                    subprocess.call("clear")
 
 
 
 
 
-print("\n                    Final Table                  ")
+print(f"\n    Total Loops: {total}                Final Table                  ")
 for i in range(9):
     if (i % 3) == 0:
         print("\n")
